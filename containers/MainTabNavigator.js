ae3e65c1d3ca78c024f7beb884b6679b01a49523
lalis <lalis@pop-os.localdomain>
2018-10-05 21:52:19 -0300

    Redux Compilacao Basica

--- containers/MainTabNavigator.js
+++ containers/MainTabNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Platform } from 'react-native';
 import { createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
 
-import HomeScreen from '../screens/HomeScreen';
+import HomeScreen from './screens/home';
 // import LinksScreen from '../screens/LinksScreen';
 // import SettingsScreen from '../screens/SettingsScreen';
 
