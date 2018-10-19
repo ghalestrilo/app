0829859aef32ba07efc9327b6ef1ed712413b5d5
Victor Guedes <victorgcramos@gmail.com>
2018-10-19 15:27:29 -0300

    added firebase module connection

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import firebase from "firebase";
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -10,6 +11,17 @@ import reducer from "./reducers/index";
 const store = createStore(reducer, {}, applyMiddleware(Logger));
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyC5hIUffHTZc7RPXPxhU8MuB85GqWAi3_w",
+      authDomain: "igor-dev-apps.firebaseapp.com",
+      databaseURL: "https://igor-dev-apps.firebaseio.com",
+      projectId: "igor-dev-apps",
+      storageBucket: "igor-dev-apps.appspot.com",
+      messagingSenderId: "463098371234"
+    };
+    firebase.initializeApp(config);
+  }
   render(){
     return (
       <Provider store={store}>
