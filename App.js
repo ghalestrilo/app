198922c9a38e201c24edc377024447b45da9b254
Victor Guedes <victorgcramos@gmail.com>
2018-10-26 14:50:15 -0300

    added src folder

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-import { AppNavigator } from "./containers/navigators";
-import reducer from "./reducers/index";
+import { AppNavigator } from "./src/containers/navigators";
+import reducer from "./src/reducers/index";
 
 const store = createStore(reducer, {}, applyMiddleware(thunk));
 
