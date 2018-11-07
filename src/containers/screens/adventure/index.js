a4dac59781d08e473a67439a37108f535ac88e86
ghalestrilo <ghales.trilo@gmail.com>
2018-11-07 17:17:54 -0200

    Begin adventure screen

--- src/containers/screens/adventure/index.js
+++ src/containers/screens/adventure/index.js
@@ -1,17 +1,78 @@
 import React from "react";
+import {
+  ImageBackground
+} from "react-native"
 import { connect } from "react-redux";
 import { SafeAreaView } from "react-native-safe-area-view";
 import { Text } from "react";
+import { Fab } from "../../../components/Igor"
 
+const boxWithTabs = require('../../../images/adventure/content-box.png')
 
+const andamento = 'andamento'
+const andamento = 'jogadores'
+
+style = {
+  title: {
+
+  },
+
+  tab: {
+
+  },
+
+  tabContainer: {
+
+  },
+
+  boxContent: {
+
+  },
+}
 
 class Adventure extends React.Component {
+  state = {
+    tab: andamento
+  }
+
+  switch()
+
   render = () => (
     <SafeAreaView>
-      
-      <Text>
-        ADVENTURE
+      <Text style={style.title}>
+        {this.props.adventure.name}
       </Text>
+
+      <ImageBackground
+        source={boxWithTabs}
+        flipped={this.state.tab === jogadores}>
+
+        <View style={style.tabContainer}>
+          <TouchableOpacity>
+            <Text style={style.tab}>ANDAMENTO</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={style.tab}>ANDAMENTO</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.boxContent}>
+          <Text>{this.props.adventure}</Text>
+
+          {this.props.adventure.sessions.map(
+            (session) =>
+              <View style={style.session}>
+                <Text>{session.date}</Text>
+                <Text>{session.title ? session.title : "Sessão sem título"}</Text>
+              </View>
+          )}
+        </View>
+      </ImageBackground>
+
+      <Fab 
+        source={newAdventureImage}
+        onPress={() => this.onNewAdventure()}/>
     </SafeAreaView>
   )
 }
